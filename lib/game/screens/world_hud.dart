e3801458288eb63ec80f0f2d58ce7d75077de820
Dreynox <admin@dreynox.com>
2026-09-21 17:16:49 -0500

    flutter-game: show live max HP MP SP in native HUD

--- lib/game/screens/world_hud.dart
+++ lib/game/screens/world_hud.dart
@@ -13,6 +13,7 @@ class WorldHud extends StatelessWidget {
   final List<String> messages;
   final bool questOpen;
   final int questId;
+  final int hp,mp,sp;
   final VoidCallback onAcceptQuest;
   final VoidCallback onCancelQuest;
 
@@ -26,6 +27,9 @@ class WorldHud extends StatelessWidget {
     required this.messages,
     required this.questOpen,
     required this.questId,
+    required this.hp,
+    required this.mp,
+    required this.sp,
     required this.onAcceptQuest,
     required this.onCancelQuest,
   });
@@ -135,17 +139,17 @@ class WorldHud extends StatelessWidget {
         ),
       ),
     ),
-    const Positioned(
+    Positioned(
       left:83,top:28,
-      child:Text('255 / 255',style:TextStyle(fontSize:8,color:Colors.white)),
+      child:Text('$hp / $hp',style:const TextStyle(fontSize:8,color:Colors.white)),
     ),
-    const Positioned(
+    Positioned(
       left:84,top:42,
-      child:Text('95 / 95',style:TextStyle(fontSize:8,color:Colors.white)),
+      child:Text('$mp / $mp',style:const TextStyle(fontSize:8,color:Colors.white)),
     ),
-    const Positioned(
+    Positioned(
       left:82,top:57,
-      child:Text('180 / 180',style:TextStyle(fontSize:8,color:Colors.white)),
+      child:Text('$sp / $sp',style:const TextStyle(fontSize:8,color:Colors.white)),
     ),
   ]);
 
