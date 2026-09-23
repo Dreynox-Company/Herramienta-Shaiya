bd2b3380a437a0e0996cbc92ce9d31112e07be5d
Dreynox <admin@dreynox.com>
2026-09-22 21:11:44 -0500

    build: compile Flutter GPU shader bundle

---
+++ hook/build.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_gpu_shaders/build.dart';
+import 'package:hooks/hooks.dart';
+
+void main(List<String> args) async {
+  await build(args, (input, output) async {
+    await buildShaderBundleJson(
+      buildInput: input,
+      buildOutput: output,
+      manifestFileName: 'shaders/shaiya_gpu.shaderbundle.json',
+    );
+  });
+}
